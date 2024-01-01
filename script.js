f4f5645d361ac29b397a779a660e967cdb63469a
soul059 <136678396+soul059@users.noreply.github.com>
2024-01-01 13:43:03 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 let svg = document.querySelector("#svg");
+//Create a link element
+var link = document.createElement('link');
+//Set the attributes for the link element
+link.rel = 'icon';
+link.type = 'svg';
+link.href = 'coding.svg'; //The URL of the image
+//Append the link element to the head section of the document
+document.head.appendChild(link);
+
 svg.addEventListener("click",()=>{
     document.body.classList.toggle("black");
     if(document.body.classList.contains("black"))
